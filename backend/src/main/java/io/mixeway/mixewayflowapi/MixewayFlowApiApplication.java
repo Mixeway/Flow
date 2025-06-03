ab2d742300a1e7575856aa49913d3651bab33c09
berejmaj <maja.berej@orange.com>
2025-06-03 12:53:48 +0200

    removal mechanism for gitlab findings

--- backend/src/main/java/io/mixeway/mixewayflowapi/MixewayFlowApiApplication.java
+++ backend/src/main/java/io/mixeway/mixewayflowapi/MixewayFlowApiApplication.java
@@ -1,8 +1,6 @@
 package io.mixeway.mixewayflowapi;
 
-import io.mixeway.mixewayflowapi.db.entity.CodeRepo;
-import io.mixeway.mixewayflowapi.db.repository.CodeRepoRepository;
-import io.mixeway.mixewayflowapi.integrations.scanner.gitlab_scanner.GitLabRules;
+import io.mixeway.mixewayflowapi.scanmanager.scheduler.StatsScheduler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,37 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @SpringBootApplication
 @EnableScheduling
 @Log4j2
 public class MixewayFlowApiApplication {
 
-    private final GitLabRules gitLabRules;
-    private final CodeRepoRepository codeRepoRepository;
-
-    public MixewayFlowApiApplication(GitLabRules gitLabRules, CodeRepoRepository codeRepoRepository) {
-        this.gitLabRules = gitLabRules;
-        this.codeRepoRepository = codeRepoRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(MixewayFlowApiApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner run() {
-        return args -> {
-            // Fetch the first repository from the database
-            Optional<CodeRepo> repoWithIdOne = codeRepoRepository.findById(1L);
-
-            if (repoWithIdOne.isPresent()) {
-                log.info("Running checkDefaultBranchProtection for repository: {}", repoWithIdOne.get().getRepourl());
-                gitLabRules.checkDefaultBranchProtection(repoWithIdOne.get());
-            } else {
-                log.warn("No repositories found in the database.");
-            }
-        };
-    }
 }
