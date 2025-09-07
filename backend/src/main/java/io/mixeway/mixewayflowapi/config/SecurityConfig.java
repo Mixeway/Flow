67c4d6982a90bbbf3a37eb89fa5dadac96a57aab
Tomek <tomasz.bondaruk@wp.pl>
2025-09-07 18:22:56 +0200

    Security alteration and data dump

--- backend/src/main/java/io/mixeway/mixewayflowapi/config/SecurityConfig.java
+++ backend/src/main/java/io/mixeway/mixewayflowapi/config/SecurityConfig.java
@@ -48,9 +48,6 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/login").permitAll()
-                        .requestMatchers("/api/v1/vulnerability/update").permitAll()
-                        .requestMatchers("/api/v1/vulnerability/update/*").permitAll()
-                        .requestMatchers("/api/v1/gemini/vulnerability").permitAll()
                         .requestMatchers("/api/v1/status").permitAll() // Ensure the SSO endpoint is public
                         .requestMatchers("/api/v1/webhook/gitlab/push").permitAll()
                         .requestMatchers("/api/v1/webhook/gitlab/merge").permitAll()
