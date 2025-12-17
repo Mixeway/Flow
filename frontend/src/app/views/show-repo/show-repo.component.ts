5fdb9395d5251646fc0a93ed3b149370bc9f0162
berejmaj <maja.berej@orange.com>
2025-12-17 16:06:17 +0100

    grype integration, CreateFindingService fixed, repository and file redirect fix, gitlab cicd branch creation fix

--- frontend/src/app/views/show-repo/show-repo.component.ts
+++ frontend/src/app/views/show-repo/show-repo.component.ts
@@ -554,7 +554,7 @@ export class ShowRepoComponent implements OnInit, AfterViewInit {
                                 '#3eabb7',
                                 '#FFCE12',
                                 '#FF8929D8',
-                                '#C34E75F4'
+                                '#679E5CDB'
                             ],
                             hoverBackgroundColor: [
                                 '#FF6384',
@@ -562,7 +562,7 @@ export class ShowRepoComponent implements OnInit, AfterViewInit {
                                 '#449a77',
                                 '#FFCE12',
                                 '#FF8929D8',
-                                '#C34E75F4'
+                                '#679E5CDB'
                             ],
                         },
                     ],
