b2ac41160e646cdc1c34a2bbd09749efc01eaf54
berejmaj <maja.berej@orange.com>
2026-04-23 12:41:39 +0200

    github webhooks fix

--- backend/src/main/java/io/mixeway/mixewayflowapi/api/webhook/dto/GHMergeEventDTO.java
+++ backend/src/main/java/io/mixeway/mixewayflowapi/api/webhook/dto/GHMergeEventDTO.java
@@ -1,34 +1,51 @@
 package io.mixeway.mixewayflowapi.api.webhook.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GHMergeEventDTO {
+
+    private String action;
+    private Long number;
+
+    @JsonProperty("pull_request")
     private PullRequestDTO pullRequest;
+
     private RepositoryDTO repository;
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class PullRequestDTO {
         private Long id;
-        private HeadDTO head;
-        private BaseDTO base;
         private String state;
 
-        @Data
-        public static class HeadDTO {
-            private String ref;
-            private String sha;
-        }
+        @JsonProperty("head")
+        private HeadBaseDTO head;
 
-        @Data
-        public static class BaseDTO {
-            private String ref;
-            private String sha;
-        }
+        @JsonProperty("base")
+        private HeadBaseDTO base;
     }
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class HeadBaseDTO {
+        private String ref;
+        private String sha;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class RepositoryDTO {
         private Long id;
+        private String name;
+
+        @JsonProperty("full_name")
+        private String fullName;
+
+        @JsonProperty("default_branch")
+        private String defaultBranch;
     }
 }
